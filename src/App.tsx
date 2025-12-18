6bf41cdecd786fba1d41eb6e42dbade999e6fb39
temogogitidze <temogogitidze@redberry.ge>
2025-12-18 14:52:31 +0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import { useBootGame } from "./hooks/useBootGame";
 import Loader from "./components/Loader";
-
 import styles from "./app.module.css";
 
 export default function App() {
     const boot = useBootGame();
 
-    if (boot.status === "loading") {
-        return (
-            <div className={styles.app}>
-                <Loader />
-            </div>
-        );
-    }
-
-    const Game = boot.Game;
-
     return (
         <div className={styles.app}>
-            <Game />
+            {boot.status === "loading" ? <Loader /> : <boot.Game />}
         </div>
     );
 }
